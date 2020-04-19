55c138b9b1eb1d5f76ae195a3eb9a8603fa46815
Tureckiy <ev.zhdanov@gmail.com>
2020-04-19 14:08:06 +0300

    hw 02 js

--- hw-02/js/task-05.js
+++ hw-02/js/task-05.js
@@ -1,69 +1,77 @@
 "use strict";
-// let cost;
-// let userChoise = prompt("Страна доставки").toLowerCase();
-// userChoise
-// switch (userChoise) {
-//   case null:
-//     alert("Отменено пользователем");
-//     break;
+// * Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку.
+// * Функция проверяет ее на содержание слов spam и sale. Если нашли зарещенное слово
+// * то функция возвращает true, если запрещенных слов нет функция возвращает false.
+// * Слова в строке могут быть в произвольном регистре.
 
-//   case "Китай".toLowerCase():
-//     cost = 100;
-//     console.log(userChoise);
-//     alert(`Доставка в Китай будет стоить ${cost} кредитов.`);
-//     break;
-
-//   case "Чили".toLowerCase():
-//     cost = 200;
-//     console.log(userChoise);
-//     alert(`Доставка в Чили  будет стоить ${cost} кредитов.`);
-//     break;
-
-//   case "Индия".toLowerCase():
-//     cost = 300;
-//     console.log(userChoise);
-//     alert(`Доставка в Индия будет стоить ${cost} кредитов.`);
-//     break;
-
-//   case "Австралия ".toLowerCase():
-//     cost = 400;
-//     console.log(userChoise);
-//     alert(`Доставка в Австралия будет стоить ${cost} кредитов.`);
-//     break;
-
-//   case "Ямайка".toLowerCase():
-//     cost = 500;
-//     console.log(userChoise);
-//     alert(`Доставка в Индия будет стоить ${cost} кредитов.`);
-//     break;
-
-//   default:
-//     alert(`Нэ робэ!`);
-// }
-
-// Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку.
-// Функция проверяет ее на содержание слов spam и sale. Если нашли зарещенное слово
-// то функция возвращает true, если запрещенных слов нет функция возвращает false.
-//  Слова в строке могут быть в произвольном регистре.
+const spams = ["spam", "sale", "boom", "lol", "fuck", "duck"];
+let back = "";
 
 const checkForSpam = function (message) {
-  const spams = ["spam", "sale"];
-  const words = message.toLowerCase().split(" ");
+  // debugger
+  let newMessage = message.toLowerCase().split(" ");
+//   let messageOutput = ' ';
+   
+//  for (const spam of spams) {
+// // console.log(messageOutput);
+// if (newMessage.includes(spam)) {
+//   return true;
+// } else {
+//   return false
+// }
 
-  for (const word of words) {
-    for (const i=0; i<spams.length; i++) 
-    console.log(spams[i]);
-    
-    //   if (word.includes(spams[i])) {
-    //     return true;
-    //   } else {
-    //     return false;    // если раскоментировать то делает билибидру - меняет 73ю строку на фолс
-    //   }
+
+// }
+// }
+ 
+ 
+  for (const spam of spams) {
+
+    // ** if ((newMessage.includes(spam)  ? "continue" : numbers.push(userInputInteger))
+    // if (newMessage.includes(spam) !== true ? 'true' : 'false')
+      if (newMessage.includes(spam)) {
+        back = "true"
+        return back;
+      } else {return false};
+      // let count="";
+      // count += spam;
+      // console.log(count);
+      
+      // console.dir(message);
     }
-  
-  // return dddd
-};
+  }
+  console.dir (back);
 
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(checkForSpam("Latest technology spa m s ale news")); // false
+
+console.log(checkForSpam("JavaScript [SPAM] weekly newsletter duc k")); // false
+
+console.log(checkForSpam("Get best sale offers now!")); // true
+
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+// =========================================================================
+
+// const checkForSpam = function (message) {
+//   const spams = ["spam", "sale"];
+//   const words = message.toLowerCase().split(" ");
+
+//   for (const word of words) {
+//     for (const i=0; i<spams.length; i++)
+//     console.log(spams[i]);
+
+//     //   if (word.includes(spams[i])) {
+//     //     return true;
+//     //   } else {
+//     //     return false;    // если раскоментировать то делает билибидру - меняет 73ю строку на фолс
+//     //   }
+//     }
+
+//   // return dddd
+// };
 
 // debugger
 
@@ -75,19 +83,19 @@ const checkForSpam = function (message) {
 //       }
 //     return
 //   }
-   
+
 // };
 
-// };  
+// };
 // for (const spam of spams) {(word.includes(spam)) ? true : false}}
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
-console.log(checkForSpam("Latest technology news")); // false
+// /*
+//  * Вызовы функции для проверки работоспособности твоей реализации.
+//  */
+// console.log(checkForSpam("Latest technology news")); // false
 
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
+// console.log(checkForSpam("JavaScript weekly newsletter")); // false
 
-console.log(checkForSpam("Get best sale offers now!")); // true
+// console.log(checkForSpam("Get best sale offers now!")); // true
 
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
--- hw-02/js/task-07.js
+++ hw-02/js/task-07.js
@@ -22,8 +22,8 @@ const addLogin = function (allLogins, login) {
 /*
  * Вызовы функции для проверки работоспособности твоей реализации.
  */
-console.log(addLogin(logins, "Ajуввax")); // 'Логин успешно добавлен!'
+console.log(addLogin(logins, "Ajуx")); // 'Логин успешно добавлен!'
 console.log(addLogin(logins, "robotGoogles")); // 'Такой логин уже используется!'
-console.log(addLogin(logins, "Zodц")); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+console.log(addLogin(logins, "Zod")); // 'Ошибка! Логин должен быть от 4 до 16 символов'
 console.log(addLogin(logins, "jqueryisextremelyfast")); // 'Ошибка! Логин должен быть от 4 до 16 символов'
 console.log(logins);
